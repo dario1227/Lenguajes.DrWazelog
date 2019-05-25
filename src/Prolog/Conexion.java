e103a3a23c7e1567a18c8ee455422694d97da1a7
Dario <darior1227@gmail.com>
2019-05-24 19:55:46 -0600

    Coneccion

--- src/Prolog/Conexion.java
+++ src/Prolog/Conexion.java
@@ -1,5 +1,61 @@
 package Prolog;
 
+
+import org.jpl7.Query;
+import org.jpl7.Term;
+
 public class Conexion {
+    String prog="consult('GLC.pl')";
+    Query q1=new Query(prog);
+    public  void test(){
+        System.out.println(prog +(q1.hasSolution()? "Coneccion completada" :"Coneccion Fallida"));
+    }
+    private String getResult(String s){
+        int x=s.length();
+        int i=0;
+        String result="";
+        while(x!=i){
+            String c= Character.toString(s.charAt(i));{
+                if (c.equals("(") || c.equals(")")|| c.equals("|")|| c.equals("'")|| c.equals(",")|| c.equals("[") || c.equals("]")){
+                    result+="";
+                }
+                else{
+                    result+=c;
+                }
+            }
+            i++;
+        }
+        return result;
+
+    }
+    public String pregunta(String s, int x){
+        String result ="No le entendi";
+        if (x==1){
+            String data="q1("+s+",X)";
+            Query q= new Query(data);
+            result= q.oneSolution().get("X").toString();
+            System.out.println(result);
+        }
+        else if(x==2){
+                String data="q2("+s+",X)";
+                Query q= new Query(data);
+                result= q.oneSolution().get("X").toString();
+                System.out.println(result);
+        }
+        else if(x==3) {
+            String data = "q3(" + s + ",X)";
+            Query q = new Query(data);
+            result = q.oneSolution().get("X").toString();
+            System.out.println(result);
+        }
+        else if(x==5) {
+            String data = "q5(" + s + ",X)";
+            Query q = new Query(data);
+            result = q.oneSolution().get("X").toString();
+            System.out.println(result);
+        }
+        return getResult(result);
+
+    }
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,23 +1,26 @@
 package sample;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+import Prolog.Conexion;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+
     }
 
-
     public static void main(String[] args) {
+        Conexion conexion= new Conexion();
+        conexion.test();
+        String d="['en','cartago']";
+        conexion.pregunta(d,1);
         launch(args);
     }
 }
