25ef82231918d11c0e6825e0024d6e21dcd1c724
Dario <darior1227@gmail.com>
2019-05-25 07:33:06 -0600

    no jeje

--- GLC.pl
+++ GLC.pl
@@ -1,8 +1,8 @@
 :-include('Lugares.pl').
 :-include('Arcos.pl').
-q1(O,X):-nominal1(Sustan),verbal1(Verbo,X),append(Sustan,Verbo,O).
-q1(X,X):-lugar(X).
-q1(O,X):-verbal1(O,X).
+q1(O,X):-nominal1(Sustan),verbal1(Verbo,X),append(Sustan,Verbo,O),!.
+q1(X,X):-lugar(X),!.
+q1(O,X):-verbal1(O,X),!.
 /*Pregunta 1*/
 nominal1(Sustan):- saludos(S),nombre(N),append(S,N,Sustan).
 nominal1(Sustan):- nombre(Sustan).
@@ -11,14 +11,14 @@ verbal1(Verbo,X):-prepo(P),lugar(X), append(P,X,Verbo).
 verbos(S):-verbo(V),prepo(P),append(V,P,S).
 saludos(S):-saludo(T),waze(F),append(T,F,S).
 /*pregunta 2*/
-q2(X,X):-lugar(X).
-q2(O,X):-verbal2(O,X).
+q2(X,X):-lugar(X),!.
+q2(O,X):-verbal2(O,X),!.
 verbal2(Verbo,X):-prepo(P),lugar(X),append(P,X,Verbo).
 /*pregunta 3*/
-q3(O,X):-verbal3(V),conec(C,X),append(V,C,O).
-q3(O,X):-conec(O,X).
-q3(X,X):-local(X).
-q3(X,X):-no(X).
+q3(O,X):-verbal3(V),conec(C,X),append(V,C,O),!.
+q3(O,X):-conec(O,X),!.
+q3(X,X):-local(X),!.
+q3(X,X):-no(X),!.
 verbal3(V):-verbo2(B),extra(P),append(B,P,V).
 extra(P):-que(C),verbor(X),append(C,X,P).
 extra(P):-verbor(P).
@@ -26,8 +26,8 @@ conec(C,Y):-prepo2(P),local(Y), append(P,Y,C).
 /*pregunta 4 */
 /*recibe lo que sea */
 /*pregunta 5 */
-q5(O,X):-verbal5(V),extra5(C,X),append(V,C,O).
-q5(O,X):-extra5(O,X).
+q5(O,X):-verbal5(V),extra5(C,X),append(V,C,O),!.
+q5(O,X):-extra5(O,X),!.
 verbal5(B):-se(S),verboL(V),append(S,V,B).
 extra5(X,Y):-prepo(P),lugar(Y),append(P,Y,X).
 se(['se']).
